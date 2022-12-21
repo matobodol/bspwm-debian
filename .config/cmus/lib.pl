1dd8e697244309f7e2ea04d529c78458fd4607c8
matobodol <matobodol@gmail.com>
2022-12-22 01:46:48 +0700

    update

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,3 @@
+/home/id/Music/KERANDA CINTA - Lusyana Jelita Adella - OM ADELLA.mp3
+/home/id/Music/Yeni Inka  PECAH SERIBU Official MV Hanya Dia Yang Ada Diantara Jantung Hati.mp3
+/home/id/Music/Janna Nick - Gatal (Official Music Video).mp3
