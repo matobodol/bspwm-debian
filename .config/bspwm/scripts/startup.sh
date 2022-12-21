1c51f99a3dd8876378e9769c0115edd753ae63d0
matobodol <matobodol@gmail.com>
2022-12-21 18:05:37 +0700

    update

--- .config/bspwm/scripts/startup.sh
+++ .config/bspwm/scripts/startup.sh
@@ -15,65 +15,64 @@ fi
 launcher-light() {
 cat > $launcher <<EOF
 * {
-	accent:					#B379F2;
-	background:				#F3F3F3;
-	foreground:				#363C41;
-	bgselected:				#FF80C3;
-	fgselected:				#FFFFFF;
+	accent:			#B379F2;
+	background:		#F3F3F3;
+	foreground:		#363C41;
+	bgselected:		#FF80C3;
+	fgselected:		#FFFFFF;
 }
 EOF
 cat > $pwm <<EOF
-* {  
-	accent:        		   	#B379F2;
-	background:     	  	#F9F9F9;
-	background-light:	 	#F0F0F0;
-	foreground:       		#FF80C3;
-	fg-selected:			#FFFFFF;
+* {
+	accent:			#B379F2;
+	background:		#F9F9F9;
+	background-light:	#F0F0F0;
+	foreground:		#FF80C3;
+	fg-selected:		#FFFFFF;
 }
 EOF
-	}	
-	
+}
+
 launcher-dark() {
 cat > $launcher <<EOF
 * {
-	accent:					#363C41;
-	background:				#15191F;
-	foreground:				#64D1E0;
-	bgselected:				#B379F2;
-	fgselected:				#FFFFFF;
+	accent:			#363C41;
+	background:		#15191F;
+	foreground:		#64D1E0;
+	bgselected:		#B379F2;
+	fgselected:		#FFFFFF;
 }
 EOF
 cat > $pwm <<EOF
 * {
-	accent:					#B379F2;
-	background:				#363C41;
-	background-light:		#15191F;
-	foreground:				#64D1E0;
-	fg-selected:			#FFFFFF;
+	accent:			#B379F2;
+	background:		#363C41;
+	background-light:	#15191F;
+	foreground:		#64D1E0;
+	fg-selected:		#FFFFFF;
 }
 EOF
-	}
+}
 
 case $THEME in
-LIGHT)	
+LIGHT)
 	sed -i 's/Net\/IconThemeName.*/Net\/IconThemeName "Cobalt"/g' $gtkconf
 	sed -i 's/Net\/ThemeName.*/Net\/ThemeName "Qogir-Light"/g' $gtkconf
-	
-	
+
 	sed -i '0,/background =.*/s//background = #f3f3f3/' $panelconf
 	sed -i '0,/foreground =.*/s//foreground = #363C41/' $panelconf
 	sed -i '0,/accent =.*/s//accent = #BDBDBD/' $panelconf
-	
+
 	launcher-light
 	;;
 DARK)
 	sed -i 's/Net\/IconThemeName.*/Net\/IconThemeName "Cobalt-dark"/g' $gtkconf
 	sed -i 's/Net\/ThemeName.*/Net\/ThemeName "Qogir-Dark"/g' $gtkconf
-	
+
 	sed -i '0,/background =.*/s//background = #15191f/' $panelconf
 	sed -i '0,/foreground =.*/s//foreground = #f3f3f3/' $panelconf
 	sed -i '0,/accent =.*/s//accent = #363C41/' $panelconf
-	
+
 	launcher-dark
 	;;
 esac
