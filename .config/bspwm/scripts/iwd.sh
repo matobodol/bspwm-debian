fd21cd07f16b81a635e7534d78f3a3692d2c8aeb
matobodol <matobodol@gmail.com>
2022-12-24 17:06:36 +0700

    update

--- .config/bspwm/scripts/iwd.sh
+++ .config/bspwm/scripts/iwd.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 source ~/.config/bspwm/globalrc
 
-ESSID=ginkgo
-PASS=1sampai10
+ESSID=
+PASS=
 
 iface=$(iwctl device list | awk '$5=="station" {print $1}' | head -n 1)
 iface_powered=$(iwctl device list | grep "$iface" | awk '{print $3}')
@@ -11,30 +11,31 @@ state=$(iwctl station $iface show | awk '$1=="State" {print $2}')
 notify() {
 	iwctl station $iface connect "$namawifi" --passphrase "$passwifi" &>/dev/null
 	if [[ $? == 0 ]]; then
-		$NOTIFY -i $ICON/info.png -t 2300 "Wifi Connected" "to $namawifi"
-		if [[ "$ESSID" != "$namawifi" ]]; then
-			sed -i "0,/ESSID=.*/s//ESSID=$namawifi/" $0
-			sed -i "0,/PASS=.*/s//PASS=$passwifi/" $0
-		fi
+		$NOTIFY -i $ICON/info.png -t 2300 -r 123 "Wifi Connected" "to $namawifi"
+		[[ "$ESSID" != "$namawifi" ]] && sed -i "0,/ESSID=.*/s//ESSID=$namawifi/" $0
+		[[ "$PASS" != "$passwifi" ]] && sed -i "0,/PASS=.*/s//PASS=$passwifi/" $0
 	else
 		iwctl station $iface connect-hidden "$namawifi" --passphrase "$passwifi" &>/dev/null
-		[[ $? == 0 ]] && $NOTIFY -i $ICON/info.png -t 2300 "Wifi Connected" "to $namawifi"
+		if [[ $? == 0 ]]; then
+			$NOTIFY -i $ICON/info.png -t 2300 -r 123 "Wifi: Connected" "to $namawifi"
+		else
+		 	$NOTIFY -i $ICON/info.png -t 2300 -r 1234 "Wifi: Unable" "to connect"
+		fi
 	fi
-	
 }
 
 wifi_connecting() {
-network=$(ip link | grep "state UP" | awk '{print $2}' | awk -F: '{print $1}')		#cek wifi koneksi
-n=15
-for i in {0..15}
+n=50
+for i in {1..50}
 do
 	pct=$(( i * 100 / n ))
 	echo XXX; echo $pct; echo "Menghubungkan..."; echo XXX
-	sleep 0.1
-	if [[ $i -eq 15 ]]; then 
-		notify
-		break
+	if [[ $i -eq 1 ]]; then
+	iwctl station $iface scan
+	elif [[ $i -eq $(($n - 5 )) ]]; then
+	notify
 	fi
+	sleep 0.01
 done | whiptail --title "NETWORK" --gauge "Please wait..." 6 55 0
 }
 
@@ -46,16 +47,19 @@ input_box() {
 	wifi_connecting
 }
 
-[[ $iface_powered == 'off' ]] && $NOTIFY -i $ICON/info.png -t 2000 "Wifi status is OFF" && exit
+[[ $iface_powered == 'off' ]] && $NOTIFY -i $ICON/info.png -t 2000 -r 123 "Wifi status is OFF" && exit
 
 case $1 in
 0)
 	if [[ $state == 'connected' ]]; then
-		iwctl station $iface disconnect
-		$NOTIFY -i $ICON/info.png -t 2000 "Wifi Disconnected"
-	else 
+		iwctl station $iface disconnect &
+		$NOTIFY -i $ICON/info.png -t 2000 -r 123 "Wifi Disconnected"
+	else
 		input_box
 	fi ;;
 1)
-	iwctl known-networks "$ESSID" forget &>/dev/null ;;
+	iwctl known-networks $ESSID forget &
+	if [[ $? -eq 0 ]]; then
+		$NOTIFY -i $ICON/info.png -t 2300 -r 123 "Forget Network \"$ESSID\""
+	fi ;;
 esac
